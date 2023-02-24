837ac1f4298fd436f905ee5a78c61a73b26e58ab
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-02-24 11:36:18 +0700

    Squashed 'Assets/DVAH/Unity3rdLib/' changes from e722a41..03c9a04
    03c9a04 fix native_item auto insnt + fix language translator + setkey function
    e08b2fa fix some small bug
    REVERT: e722a41 Merge pull request #41 from nhathuy7996/develop
    REVERT: 789f2e1 Merge pull request #40 from nhathuy7996/develop
    REVERT: 8a343dc Merge pull request #39 from nhathuy7996/develop
    REVERT: f4eaca8 Merge pull request #38 from nhathuy7996/develop
    REVERT: 5306a39 Merge pull request #37 from nhathuy7996/develop
    REVERT: 54a604b Merge pull request #36 from nhathuy7996/develop
    REVERT: 3919521 Merge pull request #35 from nhathuy7996/develop
    REVERT: 63a28ed Merge pull request #34 from nhathuy7996/develop
    REVERT: eeaea43 Merge pull request #33 from nhathuy7996/develop
    REVERT: dd5639a Merge pull request #32 from nhathuy7996/develop
    REVERT: d8d09bb Merge pull request #31 from nhathuy7996/develop
    REVERT: 35abe81 Merge pull request #30 from nhathuy7996/develop
    REVERT: f685111 Merge pull request #29 from nhathuy7996/develop
    REVERT: 03d4ba2 Merge pull request #28 from nhathuy7996/develop
    REVERT: 2411c4c Merge pull request #27 from nhathuy7996/develop
    REVERT: ea9b4b5 Merge pull request #26 from nhathuy7996/develop
    REVERT: c99adea Merge pull request #25 from nhathuy7996/develop
    REVERT: b36f73c Merge pull request #24 from nhathuy7996/develop
    REVERT: 1d32fb4 Merge pull request #23 from nhathuy7996/develop
    REVERT: cc19c6f Merge pull request #22 from nhathuy7996/develop
    REVERT: edba523 Merge pull request #21 from nhathuy7996/develop
    REVERT: 47d977d Merge pull request #20 from nhathuy7996/develop
    REVERT: bfd7afd Merge pull request #19 from nhathuy7996/develop
    REVERT: b13287e Merge pull request #18 from nhathuy7996/develop
    REVERT: a4564ee Merge pull request #17 from nhathuy7996/develop
    REVERT: db197c2 Merge pull request #16 from nhathuy7996/develop
    REVERT: 8f2ec9b Merge pull request #15 from nhathuy7996/develop
    REVERT: f3a8ee9 Merge pull request #14 from nhathuy7996/develop
    REVERT: 7ff9fb6 Merge pull request #13 from nhathuy7996/develop
    REVERT: ea75c44 Merge pull request #12 from nhathuy7996/develop
    REVERT: 5ae97a2 Merge pull request #11 from nhathuy7996/develop
    REVERT: 4f67f4a Merge pull request #10 from nhathuy7996/develop
    REVERT: 51f5b48 Merge pull request #9 from nhathuy7996/develop
    REVERT: 5fb8731 Merge pull request #8 from nhathuy7996/develop
    REVERT: dcca83e Merge pull request #7 from nhathuy7996/develop
    REVERT: 2b47eed Merge pull request #6 from nhathuy7996/develop
    
    git-subtree-dir: Assets/DVAH/Unity3rdLib
    git-subtree-split: 03c9a04a7def28bb0f966d5ab7784075f6605d88

--- Scripts/Language/LanguageManager.cs
+++ Scripts/Language/LanguageManager.cs
@@ -60,15 +60,16 @@ namespace DVAH
         {
             if (isLanguageSeted)
             {
+                ChangeLanguage((LanguageName)PlayerPrefs.GetInt("LAN"));
                 ClosePopUp();
 
             }
             else
             {
-                 
+                ChangeLanguage();
             }
 
-            ChangeLanguage((LanguageName)PlayerPrefs.GetInt("LAN"));
+            
         }
 
         public void ClosePopUp()
@@ -81,12 +82,12 @@ namespace DVAH
         public string Translator(string key)
         {
             if (!_languageDict.ContainsKey(key))
-                return key;
+                return @key;
 
             if (!_languageDict[key].ContainsKey(_currentLanguage.ToString()))
-                return key;
+                return @key;
 
-            return _languageDict[key][_currentLanguage.ToString()];
+            return @_languageDict[key][_currentLanguage.ToString()];
 
         }
 
